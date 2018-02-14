f0dbf0717c65cf01434b8b44be0c641b0f999719
micmasaphi <micmasaphi@gmail.com>
2018-02-13 22:00:52 -0500

    Matrix Factorizaion
    Added matrix_factorizaion_utilities
    
    TODO:
    > Must convert "None" data to empty for factorization.
    > Create unique ID's for row entries that are not the album urls (numeric ids are ideal)

--- metacritic_recommender/metacritic_recommender.py
+++ metacritic_recommender/metacritic_recommender.py
@@ -3,12 +3,17 @@ import numpy as np
 import os
 import webbrowser
 
-df = pd.read_json("../metacritic_crawler/critics.json")
+df_critics = pd.read_json("../output/critics.json")
+df_albums = pd.read_json("../output/albums.json")
 
-ratings_df = pd.pivot_table(df, index="publication name", columns="album name")
+# TODO: Create unique id's
+# need to differentiate based on URL -- we have too much overlap from albums with similar names
+ratings_df = pd.pivot_table(df_critics, index="publication name", columns="album id")
 
 html = ratings_df.to_html(na_rep="")
 
 with open("review_matrix.html","w") as f:
     f.write(html)
 
+#print(df_albums.loc[df_albums['name'] == "4"])
+#print(df_critics.info())
